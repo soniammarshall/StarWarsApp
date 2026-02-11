80ff7ba6b2bfb1644ce79aaa13cafb04f215ce79
Sonia M Marshall <19774884+soniammarshall@users.noreply.github.com>
2026-02-11 16:03:12 +0100

    Make details screen scrollable

--- characters/src/main/java/com/starwarsapp/characters/ui/characterdetails/CharacterDetailsScreen.kt
+++ characters/src/main/java/com/starwarsapp/characters/ui/characterdetails/CharacterDetailsScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -98,7 +100,9 @@ fun CharacterDetails(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(state = rememberScrollState())
     ) {
         CharacteristicsSection(uiState.character)
         uiState.character.films?.let {
